f96a4ee718ce52e10424b31572d8ce31b294df8c
Olawale <henry.williams658@gmail.com>
2023-07-19 03:47:48 +0000

    Add ejs as template engine

--- express_server.js
+++ express_server.js
@@ -3,6 +3,9 @@ const app = express();
 
 const PORT = 8080; // default port 8080
 
+// Set View Engine to EJS
+app.set('view engine', 'ejs');
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
