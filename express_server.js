3b1bc91df2e639c435b5e373595cf28e88d8130a
Olawale <henry.williams658@gmail.com>
2023-07-20 01:27:05 +0000

    Add route for input from users

--- express_server.js
+++ express_server.js
@@ -6,6 +6,9 @@ const PORT = 8080; // default port 8080
 // Set View Engine to EJS
 app.set('view engine', 'ejs');
 
+// To translate or parse the body of post request body
+app.use(express.urlencoded({ extended: true }));
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -26,6 +29,17 @@ app.get("/hello", (req, res) => {
 app.get("/urls", (req, res) => {
   const templateVars = {urls: urlDatabase};
   res.render("urls_index", templateVars);
+});
+
+// For user long url to input
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+// Handle form submission
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
 });
 
 // Single URL
