3b35939374f346f39aa54278abf87e1c4daf9521
Olawale <henry.williams658@gmail.com>
2023-07-20 00:18:56 +0000

    Add a new route for individual url

--- express_server.js
+++ express_server.js
@@ -28,6 +28,12 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+// Single URL
+app.get("/urls/:id", (req, res) => {
+  const templateVars = {id: req.params.id, longURL: urlDatabase[req.params.id]};
+  res.render("urls_show", templateVars);
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
 });
